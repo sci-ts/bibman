09da2a4bc95d123da63b4ea080bf7a6c5b169e56
Nikolay Chechulin <nchechulin@protonmail.com>
2023-01-05 20:35:41 +0300

    add minimal entry

--- src/entry.rs
+++ src/entry.rs
@@ -1,19 +1,44 @@
-use std::str::FromStr;
+use serde::{Deserialize, Serialize};
+use std::fs::OpenOptions;
+use std::io::Read;
+use std::path::Path;
 
-#[derive(Debug, Eq, PartialEq, Clone, Copy)]
-pub enum EntryType {
+#[derive(Debug, Eq, PartialEq, Clone, Copy, Serialize, Deserialize)]
+pub enum EntryKind {
     Article,
     Book,
 }
 
-impl FromStr for EntryType {
-    type Err = ();
+#[derive(Debug, Eq, PartialEq, Serialize, Deserialize)]
+pub struct Entry {
+    kind: EntryKind,
 
-    fn from_str(input: &str) -> Result<Self, Self::Err> {
-        match input.to_lowercase().as_str() {
-            "article" => Ok(Self::Article),
-            "book" => Ok(Self::Book),
-            _ => Err(()),
+    pub authors: Option<Vec<String>>,
+    pub title: Option<String>,
+    pub journal: Option<String>,
+    pub year: Option<u32>,
+
+    pub doi: Option<String>,
+    pub volume: Option<u32>,
+    pub pages: Option<String>,
+    pub month: Option<u32>,
+}
+
+impl Entry {
+    pub fn from_file(path: &Path) -> Result<Self, std::io::Error> {
+        let file = OpenOptions::new().read(true).open(path);
+
+        match file {
+            Err(e) => Err(e),
+            Ok(mut file) => {
+                let mut buffer = String::new();
+                file.read_to_string(&mut buffer).unwrap();
+
+                match toml::from_str(&buffer) {
+                    Err(e) => Err(e.into()),
+                    Ok(entry) => Ok(entry),
+                }
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 mod entry;
+use std::path::PathBuf;
+use entry::Entry;
+
 
 fn main() {
-    println!("Hello, world!");
+    let entry_test_file: PathBuf = ["test-data", "entry.toml"].iter().collect();
+    let entry = Entry::from_file(&entry_test_file);
+    println!("{:?}", entry);
 }
